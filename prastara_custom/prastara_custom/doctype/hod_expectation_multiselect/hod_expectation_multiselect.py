095eff9bfe2ba95ffa453ce110239e74b3e4d8a9
rahulrajeevihg <rahul@ihgind.com>
2025-06-13 13:00:56 +0400

    employee sepration in prastara

---
+++ prastara_custom/prastara_custom/doctype/hod_expectation_multiselect/hod_expectation_multiselect.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2025, PRASTARA DECORATION DESIGN L.L.C and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class HODExpectationMultiselect(Document):
+	pass
