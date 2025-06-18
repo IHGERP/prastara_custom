f95ea2cd8ee7622478e923ca1f0c83b00d7ff918
rahulrajeevihg <rahul@ihgind.com>
2025-06-18 11:32:36 +0400

    Late coming and early leaving

---
+++ prastara_custom/prastara_custom/doctype/employee_late_coming_and_early_leaving/employee_late_coming_and_early_leaving.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, PRASTARA DECORATION DESIGN L.L.C and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Employee Late Coming and Early Leaving', {
+	// refresh: function(frm) {
+
+	// }
+});
