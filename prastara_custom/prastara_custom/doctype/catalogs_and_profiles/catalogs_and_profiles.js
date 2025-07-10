1e2936f1cda50cea448fb85c67414472b7c2822c
rahulrajeevihg <rahul@ihgind.com>
2025-07-10 15:58:03 +0400

    Catalogs document

---
+++ prastara_custom/prastara_custom/doctype/catalogs_and_profiles/catalogs_and_profiles.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, PRASTARA DECORATION DESIGN L.L.C and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Catalogs and Profiles', {
+	// refresh: function(frm) {
+
+	// }
+});
