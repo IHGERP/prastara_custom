0009404410987e67517d60f932daefbecf8df5bf
rahulrajeevihg <rahul@ihgind.com>
2025-08-29 12:15:52 +0400

    Json and the prport

---
+++ prastara_custom/prastara_custom/report/project_profit_and_loss_report/project_profit_and_loss_report.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2025, PRASTARA DECORATION DESIGN L.L.C and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Project Profit And Loss Report"] = {
+	"filters": [
+
+	]
+};
